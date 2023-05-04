8d2c6b6b83263f1642f7593ca5544ea9f23e7c9c
Richard Poelderl <richard.poelderl@gmail.com>
2023-05-04 15:45:17 +0200

    ✅ subdomain route working

--- apps/nextjs/next-i18next.config.js
+++ apps/nextjs/next-i18next.config.js
@@ -3,6 +3,18 @@ module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "de"],
+    domains: [
+      {
+        domain: "localhost:3001",
+        defaultLocale: "en",
+        http: true,
+      },
+      {
+        domain: "de.localhost:3001",
+        defaultLocale: "de",
+        http: true,
+      },
+    ],
   },
   reloadOnPrerender: process.env.NODE_ENV === "development",
 };
